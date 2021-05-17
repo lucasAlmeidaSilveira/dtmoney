3f7647545795c69f622e4a7e985915800fafa0ec
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-05-17 16:07:07 -0300

    Add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
 
-import { Container, Content } from './styles'
+import { Container, Content } from './styles';
 
-export function Header(){
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+};
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
-      <img src={logoImg} alt="dt money" />
-      <button type="button">Nova Transação</button>
+        <img src={logoImg} alt='dt money' />
+        <button type='button' onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
-  )
+  );
 }
