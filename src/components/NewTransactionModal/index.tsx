7d396636d812f446d6ef828f760b6b95c90834bd
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-05-19 19:06:11 -0300

    add hooks useTransaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,10 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
-import { api } from '../../services/api';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
@@ -15,7 +14,7 @@ type NewTransactionModalProps = {
 };
 
 export function NewTransactionModal({ isOpen, onRequestClose,}: NewTransactionModalProps) {
-  const {createTransaction} = useContext(TransactionsContext)
+  const {createTransaction} = useTransactions()
 
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState(0)
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 type Transaction = {
   id: number;
@@ -21,7 +21,7 @@ type TransactionsContextData = {
   createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -53,4 +53,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions(){
+  const context = useContext(TransactionsContext);
+
+  return context
 }
