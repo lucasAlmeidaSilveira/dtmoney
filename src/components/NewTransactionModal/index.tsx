6d81c208572e438cfd985538fa2b93c5c919ade8
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-05-19 00:43:31 -0300

    map transactions in table

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 type NewTransactionModalProps = {
@@ -18,9 +19,11 @@ export function NewTransactionModal({ isOpen, onRequestClose,}: NewTransactionMo
 
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault()
-    console.log(
-      {title, value, type, category}
-    )
+    const data = {
+      title, value, type, category
+    }
+
+    api.post('/transactions', data)
   }
 
   return (
